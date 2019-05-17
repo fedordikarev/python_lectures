59b49f73d7405b6fb7f59b510625fb6b121cb7ba
Fedor Dikarev <fedor.dikarev@gmail.com>
2019-05-17 14:31:45 +0300

    Use fire

--- parse.py
+++ parse.py
@@ -1,17 +1,27 @@
 #!/usr/bin/env python3
 
 import fileinput
+import fire
 
 def parse_ctrl_list():
-    for line in fileinput.input():
+    for line in fileinput.input("-"):
         if " in Slot " not in line:
             continue
         (model, rest_line) = line.split(" in Slot ", 2)
         slot = rest_line.split()[0]
         print(slot, model)
 
+def parse_disk_list():
+    for line in fileinput.input("-"):
+        if "physicaldrive" in line:
+            (_, location) = line.split()
+            print(location)
+    pass
+
 def main():
-    parse_ctrl_list()
+    # parse_ctrl_list()
+    # parse_disk_list()
+    fire.Fire()
 
 if __name__ == "__main__":
     main()
