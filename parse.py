509c896d5a5bca2bf5497a0c21e0802789b9e253
Fedor Dikarev <fedor.dikarev@gmail.com>
2019-05-17 14:46:36 +0300

    Parse disks

--- parse.py
+++ parse.py
@@ -12,11 +12,31 @@ def parse_ctrl_list():
         print(slot, model)
 
 def parse_disk_list():
+    result = dict()
+    pd = None
+    fields = set(("Size", "Disk Name", "Model", "Serial Number"))
     for line in fileinput.input("-"):
+        line = line.strip()
         if "physicaldrive" in line:
-            (_, location) = line.split()
-            print(location)
-    pass
+            (_, pd) = line.split()
+            result[pd] = dict()
+            continue
+        if ":" in line:
+            (key, value) = line.split(": ")
+            if key not in fields:
+                continue
+            result[pd][key] = value
+            # print(">", key, "<>", value, "<")
+
+    result_by_name = dict()
+    for (location, data) in result.items():
+        print(data)
+        name = data['Disk Name']
+        data["Location"] = location
+        data.pop('Disk Name')
+        result_by_name[name] = data
+
+    return result_by_name
 
 def main():
     # parse_ctrl_list()
